1c03a3c6c42c81555dd38a87cb59f2405ee9e466
Arjo1771 <demetre.arjania@gmail.com>
2024-05-27 01:29:22 +0300

    cleaned api.js file

--- react-weather-app/src/api.js
+++ react-weather-app/src/api.js
@@ -1,15 +1,3 @@
-//export const geoApiOptions = {
-    //    method: 'GET',
-      //  headers: {
-        //    'x-rapidapi-key': '680ce5973emsh6abcb2eb8576286p1911c5jsna20dd8944af5',
-          //  'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
-       // }
-  //  };
-
-
-
-//export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-
 export const WEATHER_API_URL = "https://api.openweathermap.org";
 
 export const WEATHER_API_KEY = "e6c7c15c885daf32d70efc98abf58eea";
